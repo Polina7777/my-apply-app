c6e4a01292b89fae6c65406f9eeff21288eb827f
polina <polina@mail.ru>
2023-01-24 17:28:09 +0300

    feat:add pages

---
+++ src/ui/pages/Pages-interface.tsx
@@ -0,0 +1,4 @@
+export interface PagesProps {
+  page: number;
+  maxPage?: string | number;
+}
---
+++ src/ui/pages/Pages.tsx
@@ -0,0 +1,25 @@
+import { useAppSelector, useTypedDispatch } from 'store/Hooks';
+import { PagesProps } from './Pages-interface';
+import './Pages.css';
+
+export const Pages: React.FunctionComponent<PagesProps> = (props) => {
+  const data = useAppSelector((state) => state.info);
+  const dispatch = useTypedDispatch();
+  const dispatchHandler = (value: MouseEvent | string) => {
+    dispatch({ type: 'changePage', payload: Number(value) });
+  };
+
+  return (
+    <div className="pages">
+      <p className="pages-number">{`${data.api.page} from ${props.maxPage}  `}</p>
+      <input
+        type="search"
+        name="page"
+        className="set-page"
+        onChange={(event) => dispatchHandler(event.target.value)}
+        placeholder={'page'}
+      ></input>
+    </div>
+  );
+};
+export default Pages;
