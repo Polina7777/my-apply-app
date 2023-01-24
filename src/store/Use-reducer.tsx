d41b8d0e20b2def19f31b1e8a27906e7bb024b20
polina <polina@mail.ru>
2023-01-24 20:51:20 +0300

    feat:add use reducer

---
+++ src/store/Use-reducer.tsx
@@ -0,0 +1,63 @@
+// import { AnyAction } from '@reduxjs/toolkit';
+
+import { AnyAction } from "redux";
+
+export interface ColorData {
+  color: string;
+  id: number;
+  name: string;
+  pantone_value: string;
+  year: number;
+}
+
+export interface AppContext {
+ 
+  colors: ColorData[];
+}
+export type SliceState = {
+
+  colors: ColorData [];
+};
+export const initialState = {
+  colors: [],
+
+} as SliceState;
+
+
+export const SecondReducer = (state = initialState, action: AnyAction) => {
+  if (!state) {
+    return initialState;
+  }
+  switch (action.type) {
+ 
+    case "getAllColors":
+      return { ...state, colors: action.payload };
+
+    // case "changePage":
+    //   return { ...state, api: { ...state.api, page: action.payload } };
+    // case "getCharacter":
+    //   return {
+    //     ...state,
+    //     characterArray: { ...state.api, characterArray: action.payload },
+    //   };
+    // case "filterByGender":
+    //   return {
+    //     ...state,
+    //     api: { ...state.api, filterByGender: action.payload },
+    //   };
+    // case "filterBySpecies":
+    //   return {
+    //     ...state,
+    //     api: { ...state.api, filterBySpecies: action.payload },
+    //   };
+    // case "filterByStatus":
+    //   return {
+    //     ...state,
+    //     api: { ...state.api, filterByStatus: action.payload },
+    //   };
+    // case " clearAll":
+    //   return { ...state, api: initialState.api };
+    default:
+      return state;
+  }
+};
