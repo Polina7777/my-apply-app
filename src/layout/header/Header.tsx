70f73aca62f473f7fa022fa3a5255f00124430c8
polina <polina@mail.ru>
2023-01-24 20:52:31 +0300

    fix:fix header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { HeaderProps } from './Header-interface';
+// import { Link } from 'react-router-dom';
+// import { HeaderProps } from './Header-interface';
 import './Header.css';
 
-class Header extends React.Component<HeaderProps> {
-  render() {
+const Header = () => {
+  
     return (
       <header className="header">
-        {
-          <div className="header_text">
-            {/* <h2 className="header_title">Book</h2> */}
-            <div className="links">
-              <Link aria-label="Link" to="" className="book">
-                Book
-              </Link>
-              <Link aria-label="Link" to="about-us" className="about-us">
-                About Us
-              </Link>
-              <Link aria-label="Link-to-form" to="form-page" className="form-page_link">
-                Registration form
-              </Link>
-              <Link aria-label="Link-to-form" to="api-page" className="api-page_link">
-                Api
-              </Link>
-            </div>
-          </div>
-        }
-      </header>
+      <h1 className='header_title'>Colors...</h1>
+            </header>
     );
   }
-}
+
 export default Header;
